8e9a665c93764cd1897ed556e063ebc3d00b3cb5
cl3ment_ch <chienher@hotmail.com>
2024-11-07 22:56:11 +0800

    chore:

--- src/app/dashboard/user/list/[slug]/page.tsx
+++ src/app/dashboard/user/list/[slug]/page.tsx
@@ -1,15 +1,15 @@
 import FormPage from "./_components/form-page";
 
-export function generateMetadata({ searchParams }: any) {
-  const userId = searchParams.userid;
+export async function generateMetadata({ searchParams }: any) {
+  const userId = (await searchParams).userid;
   return {
     title: `Dashboard : User${userId ? ` (${userId})` : ""}`,
   };
 }
 
-export default function Page({ params, searchParams }: any) {
-  const userId = searchParams.userid;
-  const slug = params.slug;
+export default async function Page({ params, searchParams }: any) {
+  const userId = (await searchParams).userid;
+  const slug = (await params).slug;
 
   return <FormPage slug={slug} userId={userId} />;
 }
