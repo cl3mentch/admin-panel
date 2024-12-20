779dc92cf53455ce3c66f96f85a353b0317661b6
Clement Chong <chienher@hotmail.com>
2024-12-20 19:30:10 +0800

    feat:

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -248,7 +248,7 @@ export function SidebarFooterContent() {
               <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={"/img/profile.avif"} alt={"profile"} />
+                    <AvatarImage src={"/img/profile.gif"} alt={"profile"} />
                     <AvatarFallback className="rounded-lg">
                       {"CN"}
                     </AvatarFallback>
